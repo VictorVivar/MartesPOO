d669b4cd4cc910b68050e23ed3d2e326157c7106
Victor-Vivar <roxas.uchiha04@gmail.com>
2023-06-14 00:30:57 -0600

    se agrega el proyecto

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from Aritmetica import suma, resta
+
+
+def main():
+    resultado = suma(3, None)
+    print(resultado)
+    print(resta(b=5, a=3))
+
+main()
+
+
+def resta(a = 0, b = 1):
+    return a - b
